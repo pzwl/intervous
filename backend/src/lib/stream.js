785ac433929735edc831324851fb43df043d085c
pzwl <sharmaprajjwal333@gmmail.com>
2025-11-29 19:06:28 +0530

    trying to fix 503 #2

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,19 +1,4 @@
 import {StreamChat} from 'stream-chat';
-import {ENV} from './env.js';
-
-const apiKey = ENV.STREAM_API_KEY;
-const apiSecret = ENV.STREAM_API_SECRET;
-
-
-if(!apiKey || !apiSecret){
-    console.error("STREAM_API_KEY or STREAM_API_SECRET is not defined");
-    process.exit(1);
-}
-
-
-export const chatClient = StreamChat.getInstance({apiKey, apiSecret});
-
-
 export const upsertUser = async (user) => { // upsert meaning to insert or update
     try{
         await chatClient.upsertUser(user);

2d31077efb4b43c48c21fbb271cfc413bad3a24b
pzwl <sharmaprajjwal333@gmmail.com>
2025-11-29 18:52:08 +0530

    trying to fix 503 error

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -18,22 +18,22 @@ export const upsertUser = async (user) => { // upsert meaning to insert or updat
     try{
         await chatClient.upsertUser(user);
         console.log(`Upserted user ${user.id} to Stream`);
-    } catch(error){
+    } catch (error){
         console.error("Error upserting users to Stream:", error);
     }
 
-}
+};
 
 
 export const deleteUser = async (userID) => { // upsert meaning to insert or update
     try{
         await chatClient.deleteUser(userID);
         console.log(`Deleted user ${userID} from Stream`);
-    } catch(error){
+    } catch (error){
         console.error("Error deleting user from Stream:", error);
     }
 
-}
+};
 
 
 // todo : add another 

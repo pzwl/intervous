0f8d8b1f8c3c38722bccce3eac52132ff884b572
pzwl <sharmaprajjwal333@gmmail.com>
2025-12-05 05:45:02 +0530

    problem detail page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import { useUser } from '@clerk/clerk-react';
 import { Toaster } from 'react-hot-toast';
 import DashBoardPage from './pages/DashboardPage.jsx';
 import ProblemsPage from './pages/ProblemsPage.jsx';
+import ProblemPage from './pages/ProblemPage.jsx';
 function App() {
 
 
@@ -23,6 +24,7 @@ function App() {
         <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
         <Route path="/dashboard" element={isSignedIn ? <DashBoardPage /> : <Navigate to="/" />} />
         <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+        <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
       </Routes>
 
       <Toaster />
